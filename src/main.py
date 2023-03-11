5b3387ba21ef36aa72639f0ae8f4a5dad5dd8fd2
SOGLIUZZO Loris <sogliuzzo.loris@gmail.com>
2023-03-11 10:32:15 +0100

    Modification totale du system de vérification pour pouvoir avancer un pion, suppression du réseau de neurones créé précédemment.

--- src/main.py
+++ src/main.py
@@ -2,11 +2,9 @@ from plateau import *
 import ia.deeplearning.ia as dl
 import ia.minimax.ia as mm
 
-
 if __name__ == "__main__":
     plateau = Plateau()
 
-    coups = coups_possibles(plateau.positions(), 1)
+    positions = plateau.positions()
+    coups = coups_possibles(positions, 0)
     print(f"Nombres de coups: {len(coups)}. Coups possibles:{coups}")
-    model = dl.NeuralNetwork()
-    dl.make_prediction(plateau, model)
--- src/plateau.py
+++ src/plateau.py
@@ -15,10 +15,10 @@ class Plateau:
     def __init__(self):
         self.pions = []
         for i in range(1, 21):
-            self.pions.append(Pion(i, 0))
+            self.pions.append(Pion(i, 1))
 
         for i in range(31, 51):
-            self.pions.append(Pion(i, 1))
+            self.pions.append(Pion(i, 0))
 
     def positions(self):
         positions = {}
@@ -35,24 +35,37 @@ def coups_possibles(positions: dict, couleur=0):
     :return: Liste des coups possibles.
     """
     coups = []
-    for position in positions.keys():
-        # Si le pion est blanc
-        if not positions[position][0] and not couleur:
-            # Coup pour un point
-            if not positions[position][1]:
-                # Devant droite et gauche
-                if not (position + 5 in positions) and position // 5:
-                    coups.append(f"{position}-{position + 5}")
-                if not (position + 6 in positions) and position % 5:
-                    coups.append(f"{position}-{position + 6}")
-
-        # Si le pion est noir
-        if positions[position][0] and couleur:
-            # Coup pour un point
-            if not positions[position][1]:
-                # Devant droite et gauche
-                if not (position - 5 in positions) and position // 5 - 1:
-                    coups.append(f"{position}-{position - 5}")
-                if not (position - 6 in positions) and position % 5 - 1:
-                    coups.append(f"{position}-{position - 6}")
+    # Avancer
+    if len(coups) == 0:
+        for case in positions.keys():
+            # Pions noirs
+            if positions[case][0] and couleur == 1:
+                # Pions sur la rangée de droite et sur une ligne paire
+                # Pions sur la rangée de gauche et sur une ligne impaire
+                if (case // 5) % 2 == 1 and case % 5 == 0 or (case // 5) % 2 == 1 and case % 5 == 1:
+                    if case + 5 not in positions: coups.append(f'{case}-{case + 5}')
+                # Tous les autres pions
+                else:
+                    parity = (case // 5) % 2
+                    if parity == 0 and case % 5 == 0:
+                        parity = 1
+                    if case + 5 - parity not in positions:
+                        coups.append(f'{case}-{case + 5 - parity}')
+                    if case + 6 - parity not in positions:
+                        coups.append(f'{case}-{case + 6 - parity}')
+            # Pions blancs
+            elif positions[case][0] == 0 and couleur == 0:
+                # Pions sur la rangée de droite et sur une ligne paire
+                # Pions sur la rangée de gauche et sur une ligne impaire
+                if (case // 5) % 2 == 1 and case % 5 == 0 or (case // 5) % 2 == 1 and case % 5 == 1:
+                    if case - 5 not in positions: coups.append(f'{case}-{case - 5}')
+                # Tous les autres pions
+                else:
+                    parity = (case // 5) % 2
+                    if parity == 0 and case % 5 == 0:
+                        parity = 1
+                    if case - 5 - parity not in positions:
+                        coups.append(f'{case}-{case - 5 - parity}')
+                    if case - 4 - parity not in positions:
+                        coups.append(f'{case}-{case - 4 - parity}')
     return coups
