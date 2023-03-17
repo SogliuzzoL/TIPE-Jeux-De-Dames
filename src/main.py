4c74867111912dd5e27ffc2f4d3d704df9efdd3e
SOGLIUZZO Loris <sogliuzzo.loris@gmail.com>
2023-03-17 19:31:42 +0100

    Création de la fonction manger: - Création de la vérification pour les lignes impaires - Ajout d'un arbre afin de mieux stocker les coups possibles pour manger - Le résultat est ajouté aux coups possibles - ATTENTION SEULEMENT LE FONCTIONNEMENT DES PIONS FONCTIONNE ACTUELLEMENT, attendre une prochaine version afin de pouvoir faire déplacer les dames

--- src/main.py
+++ src/main.py
@@ -6,5 +6,6 @@ if __name__ == "__main__":
     plateau = Plateau()
 
     positions = plateau.positions()
-    coups = coups_possibles(positions, 0)
+    positions = {22: [1, False], 6: [0, False], 9: [0, False], 10: [0, False], 17: [0, False], 18: [0, False], 20: [0, False], 27: [0, False], 28: [0, False], 29: [0, False], 36: [0, False], 39: [0, False], 50: [0, False]}
+    coups = coups_possibles(positions, 1)
     print(f"Nombres de coups: {len(coups)}. Coups possibles:{coups}")
--- src/plateau.py
+++ src/plateau.py
@@ -1,3 +1,9 @@
+import time
+
+import treelib.exceptions
+from treelib import Tree
+
+
 class Pion:
     def __init__(self, emplacement, couleur):
         """
@@ -27,6 +33,33 @@ class Plateau:
         return positions
 
 
+def manger(case: int, positions: dict, couleur=0, tree=None, parent=None):
+    if tree is None:
+        tree = Tree()
+        tree.create_node(case, case)
+        parent = case
+    parity = ((case - 1) // 5) % 2
+    if parity:
+        pass
+    else:
+        datas = [[6, 11, 0], [5, 9, -1], [-4, -9, 0], [-5, -11, -1]]
+        for i in range(len(datas)):
+            if case + datas[i][0] in positions.keys() and case + datas[i][1] not in positions.keys() and (
+                    case + datas[i][2]) % 5 != 0:
+                if positions[case + datas[i][0]][0] != couleur and 0 < case + datas[i][0] < 50:
+                    positions_copie = positions.copy()
+                    data = positions_copie[case]
+                    del positions_copie[case + datas[i][0]]
+                    positions_copie[case + datas[i][1]] = data
+                    try:
+                        tree.create_node(f'{case}x{case + datas[i][1]}', f'{case}x{case + datas[i][1]}', parent=parent)
+                    except treelib.exceptions.DuplicatedNodeIdError:
+                        tree.create_node(f'{case}x{case + datas[i][1]}-{time.time()}',
+                                         f'{case}x{case + datas[i][1]}-{time.time()}', parent=parent)
+                    manger(case + datas[i][1], positions_copie, couleur, tree, f'{case}x{case + datas[i][1]}')
+    return tree
+
+
 def coups_possibles(positions: dict, couleur=0):
     """
     Cette fonction retourne tous les coups possibles en fonctions des positions des autres pions.
@@ -35,6 +68,26 @@ def coups_possibles(positions: dict, couleur=0):
     :return: Liste des coups possibles.
     """
     coups = []
+    coups_tempo = []
+    max_coups = 0
+    # Manger
+    for case in positions.keys():
+        tree = manger(case, positions, couleur)
+        if tree.depth():
+            tree.show()
+            paths = tree.paths_to_leaves()
+            for path in paths:
+                if max_coups < len(path):
+                    max_coups = len(path)
+                coups_tempo.append(path)
+    # Trie des coups pour manger
+    for i in range(len(coups_tempo)):
+        if len(coups_tempo[i]) == max_coups:
+            for j in range(len(coups_tempo[i])):
+                if '-' in str(coups_tempo[i][j]):
+                    split = str(coups_tempo[i][j]).split('-')
+                    coups_tempo[i][j] = split[0]
+            coups.append(coups_tempo[i])
     # Avancer
     if len(coups) == 0:
         for case in positions.keys():
@@ -46,9 +99,7 @@ def coups_possibles(positions: dict, couleur=0):
                     if case + 5 not in positions: coups.append(f'{case}-{case + 5}')
                 # Tous les autres pions
                 else:
-                    parity = (case // 5) % 2
-                    if parity == 0 and case % 5 == 0:
-                        parity = 1
+                    parity = ((case - 1) // 5) % 2
                     if case + 5 - parity not in positions:
                         coups.append(f'{case}-{case + 5 - parity}')
                     if case + 6 - parity not in positions:
