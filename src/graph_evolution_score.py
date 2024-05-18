d9dbf2aa1d41c4727f15857607f304729c2052fb
Loris Sogliuzzo <sogliuzzo.loris@gmail.com>
2024-05-18 20:09:40 +0200

    Affichage des statistiques

---
+++ src/graph_evolution_score.py
@@ -0,0 +1,53 @@
+from matplotlib import pyplot as plt
+
+file_name = "score.csv"
+sep = ";"
+
+gen = []
+moyenne_blancs = []
+moyenne_noirs = []
+mediane_blancs = []
+mediane_noirs = []
+ecart_type_blancs = []
+ecart_type_noirs = []
+
+n = 100
+
+with open(file_name, 'r') as file:
+    for line in file:
+        line_sep = line.split(sep)
+        gen.append(int(line_sep[0]))
+        moyenne_blancs.append(float(line_sep[1]))
+        moyenne_noirs.append(float(line_sep[2]))
+        mediane_blancs.append(float(line_sep[3]))
+        mediane_noirs.append(float(line_sep[4]))
+        ecart_type_blancs.append(float(line_sep[5]))
+        ecart_type_noirs.append(float(line_sep[6].replace("\n", "")))
+
+
+def lissage_courbe(l):
+    moyenne_glissante = []
+    for i in range(len(l) - n):
+        somme = 0
+        for j in range(n):
+            somme += l[i + j]
+        somme /= n
+        moyenne_glissante.append(somme)
+    return moyenne_glissante
+
+
+fig, ax = plt.subplots(3, 1)
+
+ax[0].plot(gen[:len(gen) - n], lissage_courbe(moyenne_blancs), label="blancs")
+ax[0].plot(gen[:len(gen) - n], lissage_courbe(moyenne_noirs), label="noirs")
+ax[0].set_title('Moyenne du score en fonction de la génération')
+
+ax[1].plot(gen[:len(gen) - n], lissage_courbe(mediane_blancs), label="blancs")
+ax[1].plot(gen[:len(gen) - n], lissage_courbe(mediane_noirs), label="noirs")
+ax[1].set_title('Médiane du score en fonction de la génération')
+
+ax[2].plot(gen[:len(gen) - n], lissage_courbe(ecart_type_blancs), label="blancs")
+ax[2].plot(gen[:len(gen) - n], lissage_courbe(ecart_type_noirs), label="noirs")
+ax[2].set_title('Ecart-type du score en fonction de la génération')
+
+plt.show()
